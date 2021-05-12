8ee0e70c3607f5d1d1a63b173bfdf3ed24abab71
Fábio L <fflucas@hotmail.com>
2021-05-12 01:38:28 -0300

    uso de loggers

--- src/main/java/br/com/zupacademy/fabio/proposta/client/card/CardsSchedule.java
+++ src/main/java/br/com/zupacademy/fabio/proposta/client/card/CardsSchedule.java
@@ -3,6 +3,8 @@ package br.com.zupacademy.fabio.proposta.client.card;
 import br.com.zupacademy.fabio.proposta.client.Proposta;
 import br.com.zupacademy.fabio.proposta.shared.TransactionExecutor;
 import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,7 @@ public class CardsSchedule {
 
     private RequestAccount requestAccount;
     private TransactionExecutor transactionExecutor;
+    private final Logger logger = LoggerFactory.getLogger(CardsSchedule.class);
 
     @Autowired
     public CardsSchedule(RequestAccount requestAccount, TransactionExecutor transactionExecutor) {
@@ -24,6 +27,9 @@ public class CardsSchedule {
     @Scheduled(fixedDelay = 10000)
     public void checkForCards(){
         List<Long> id_propostas = transactionExecutor.findPropostaElegivelSemNumeroCartao();
+        if(id_propostas.size()>0){
+            logger.info("{} propostas waiting for card numbers", id_propostas.size());
+        }
 
         for( Long id_proposta : id_propostas){
             try{
@@ -32,7 +38,9 @@ public class CardsSchedule {
                 Proposta proposta = transactionExecutor.find(Proposta.class, id_proposta);
                 proposta.setCardNumber(card.getId());
                 transactionExecutor.mergeAndCommit(proposta);
+                logger.info("Proposta {} received card number {}", proposta.getId(), proposta.getCardNumber());
             }catch (FeignException fe){
+                logger.info("Proposta {} has not yet received the card number", id_proposta);
                 System.out.println(fe.getLocalizedMessage());
             }
         }
